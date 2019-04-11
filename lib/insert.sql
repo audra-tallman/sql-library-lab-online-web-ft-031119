c7200906bfb885521a38787454c6aa982bfe7da4
Audra Tallman <audi.tallman@gmail.com>
2019-04-11 23:51:03 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,11 +1,15 @@
-INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "Harry Potter", 1, 1), (2, "Lord of the Rings", 2, 2);
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "A Song of Ice and Fire", 1, 1), (2, "Second Series", 2, 2);
 
-INSERT INTO books (id, title, year, series_id) VALUES (1, "Philosopher's Stone", 1997, 1), (2, "Chamber of Secrets", 1998, 1), (3, "Prisoner of Azkaban", 1999, 1), (4, "The Fellowship of the Ring", 1954, 2), (5, "The Two Towers", 1954, 2), (6, "Return of the King", 1955, 2);
+INSERT INTO subgenres (id, name) VALUES (1, "medieval"), (2, "space opera");
 
-INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, "Harry Potter", "Not like Voldemort", "wizard", 1, 1), (2, "Ron", "I love my rat", "wizard", 1, 1), (3, "Hermoine", "I know everything", "muggle", 1, 1), (4, "Frodo", "Destroy the ring", "", 2, 2), (5, "Aragorn", "Save Frodo", "human", 2, 2), (6, "Legolas", "Stealth elf", "elf", 2, 2), (7, "Gimli", "Elf hater", "dwarf", 2, 2), (8, "Gandalf", "You shall not pass", "wizard", 2, 2);
+INSERT INTO authors (id, name) VALUES (1, "George R. R. Martin"), (2, "Second Author");
 
-INSERT INTO subgenres (id, name) VALUES (1, "magic"), (2, "mythical creatures");
+INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 1996, 1), (2, "A Clash of Kings", 1998, 1), (3, "A Storm of Swords", 2000, 1), (4, "First Book", 2002, 2), (5, "Second Book", 2003, 2), (6, "Third Book", 2005, 2);
 
-INSERT INTO authors (id, name) VALUES (1, "J. K. Rowling"), (2, "C.S. Lewis");
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, "Lady", "Woof Woof", "direwolf", 1, 1), (2, "Tyrion Lanister", "A Lanister always pays is debts", "human", 1, 1), (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1, 1), (4, "Eddard Stark", "Winter is coming", "human", 1, 1);
 
-INSERT INTO character_books (id character_id, book_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 2, 3), (5, 3, 1), (6, 3, 3), (7, 4, 4), (8, 4, 5), (9, 5, 5), (10, 5, 6), (11, 6, 4), (12, 6, 6), (13, 7, 4), (14, 7, 5), (15, 8, 5), (16, 8, 6);
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (5, "Character One", "motto one", "cylon", 2, 2), (6, "Character Two", "motto two", "human", 2, 2), (7, "Character Three", "motto three", "cylon", 2, 2), (8, "Character Four", "motto four", "cylon", 2, 2);
+
+INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 3, 2), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 1, 4);
+
+INSERT INTO character_books (id, book_id, character_id) VALUES (9, 4, 5), (10, 4, 6), (11, 5, 6), (12, 6, 6), (13, 4, 7), (14, 5, 7), (15, 6, 7), (16, 4, 8);
