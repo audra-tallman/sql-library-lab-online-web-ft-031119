be93a184a3457af2cedfbcc1f6b453f5de9220b0
Audra Tallman <audi.tallman@gmail.com>
2019-04-12 00:09:40 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characteres.species "
+  "SELECT characteres.species FROM characteres ORDER BY "
 end
 
 def select_name_and_series_subgenres_of_authors
