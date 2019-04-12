6ffe5c95cd58f79df39dc0b515117f0286b3217a
Audra Tallman <audi.tallman@gmail.com>
2019-04-12 00:31:36 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -6,7 +6,7 @@ INSERT INTO authors (id, name) VALUES (1, "J.K. Rowling"), (2, "C.S. Lewis");
 
 INSERT INTO books (id, title, year, series_id) VALUES (1, "Philosophers Stone", 1996, 1), (2, "Chamber of Secrets", 1998, 1), (3, "Prisoner of Azkaban", 1999, 1), (4, "Fellowship of the Ring", 1954, 2), (5, "Two Towers", 1954, 2), (6, "Return of the King", 1955, 2);
 
-INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, "Harry", "Patronus", "wizard", 1, 1), (2, "Ron", "I love my rat scampers", "wizard", 1, 1), (3, "Hermoine", "I know everything", "muggle", 1, 1), (4, "Eddard Stark", "Winter is coming", "human", 1, 1);
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, "Harry", "Patronus", "wizard", 1, 1), (2, "Ron", "I love my rat scampers", "wizard", 1, 1), (3, "Hermoine", "I know everything", "muggle", 1, 1), (4, "Voldemort", "Winter is coming", "human", 1, 1);
 
 INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (5, "Character One", "motto one", "cylon", 2, 2), (6, "Character Two", "motto two", "human", 2, 2), (7, "Character Three", "motto three", "cylon", 2, 2), (8, "Character Four", "motto four", "cylon", 2, 2);
 
